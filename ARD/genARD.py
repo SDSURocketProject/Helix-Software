841fcbc62277152acd886654c9d1343d3fe9f1e0
David Knight <DKnight96@sbcglobal.net>
2019-11-04 08:40:12 -0800

    Modified ARD script to run xelatex after successfuly latex generation

--- ARD/genARD.py
+++ ARD/genARD.py
@@ -3,11 +3,15 @@ import genEEPROM
 import genCANJSON as genCAN
 
 if __name__ == "__main__":
-
+    os.chdir("ARD/")
     latexCAN, headerCAN = genCAN.getConfig("config/CAN.json")
     with open ("tex/CANIDs.tex", "w") as CAN_TEX:
         CAN_TEX.write(latexCAN)
     with open ("include/CANIDs.h", "w") as CAN_HEADER:
         CAN_HEADER.write(headerCAN)
+    if (os.system("xelatex --halt-on-error ARD.tex") != 0):
+        print("Failed to create ARD.pdf.")
+    else:
+        print("ARD.pdf successfully written.")
     #with open ("tex/EEPROM.tex", 'w') as EEPROM_TEX:
     #    EEPROM_TEX.write(genEEPROM.genEEPROM("config/EEPROM.cfg"))
