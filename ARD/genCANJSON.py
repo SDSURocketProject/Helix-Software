6e51f15ef4d8a95e8ba40fdc02a584efe87f83d8
David Knight <DKnight96@sbcglobal.net>
2019-11-04 09:11:42 -0800

    Implemented CAN config parsing from json file

--- ARD/genCANJSON.py
+++ ARD/genCANJSON.py
@@ -1,28 +1,74 @@
 import json, os, genGeneric
 
 def getConfig(filePath):
-    latexOut = ""
+    texOut = ""
     headerOut = ""
-    latexOut += genGeneric.autogenWarnStart("CAN Config", os.path.abspath(__file__))
+    texOut += genGeneric.autogenWarnStart("CAN Config", os.path.abspath(__file__))
     headerOut += genGeneric.autogenWarnStart("CAN Config", os.path.abspath(__file__), commentChar="//")
 	
     with open(filePath) as canIDs:
         data = json.load(canIDs)
     
+    texOut += "\section{CAN IDs}\n"
     for ID in data:
-        latexOut += genCANTex(ID)
+        texOut += genCANTex(ID)
     
-    latexOut += genGeneric.autogenWarnEnd("CAN Config", os.path.abspath(__file__))
+    
+    texOut += genGeneric.autogenWarnEnd("CAN Config", os.path.abspath(__file__))
     headerOut += genGeneric.autogenWarnEnd("CAN Config", os.path.abspath(__file__), commentChar="//")
-    return (latexOut, headerOut)
+    return (texOut, headerOut)
 
 def genCANTex(config):
     texOut = ""
 
     texOut += "\subsection{ID " + config["CANID"] + " - " + config["CANID_NAME"] +"}\n"
-    texOut += "Frequency: " + config["CANID_FREQUENCY"] + "Hz\n"
-    texOut += "\setlength{\LTleft}{0pt}\n"
+    texOut += "Frequency: " + config["CANID_FREQUENCY"] + "Hz\\\\\n"
+    texOut += "\\begin{tabular}{ |l|l|l|l|l|l| }\hline\n"
+    texOut += "Byte & Bit & Signed & Range & Units & Description\\\\\hline\n"
+    
+    byteIdxLow = 0
+    byteIdxHigh = 0
+    ByteStr = ""
+    BitStr = ""
+    SignedStr = ""
+    UnitsStr = ""
+    DescriptionStr = ""
+    for byteDef in config["bytes"]:
+        if ("Size" in byteDef):
+            byteIdxHigh = byteIdxLow+byteDef["Size"]-1
+            if (byteIdxHigh == byteIdxLow):
+                texOut += str(byteIdxHigh) + " & "
+            else:
+                texOut += str(byteIdxLow) + "-" + str(byteIdxHigh) + " & "
+            byteIdxLow = byteIdxLow+byteDef["Size"]
+        else:
+            texOut += " & "
+        texOut += " & " # Bit
+        if ("Signed" in byteDef):
+            texOut += byteDef["Signed"] + " & "
+        else:
+            texOut += " & "
+        if ("MinValue" in byteDef and "MaxValue" in byteDef):
+            texOut += str(byteDef["MinValue"]) + " to " + str(byteDef["MaxValue"]) + " & "
+        else:
+            texOut += " & "
+        if ("Units" in byteDef):
+            texOut += byteDef["Units"] + " & "
+        else:
+            texOut += " & "
+        if ("Name" in byteDef):
+            texOut += byteDef["Name"]
+        else:
+            texOut += " & "
 
+        texOut += "\\\\\hline\n"
+        if ("bits" in byteDef):
+            for bitDef in byteDef["bits"]:
+                for i in bitDef:
+                    texOut += " & " + i + " & & & & " + bitDef[i] + "\\\\\hline\n"
+        
+    
+    texOut += "\end{tabular}\n"
     return texOut
 
 def genCANHeader(config):
