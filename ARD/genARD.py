cb663df3bf7a4de5441692acb2538bf141fccb2b
David Knight <DKnight96@sbcglobal.net>
2019-11-25 19:49:45 -0800

    Modified ARD header locations

--- ARD/genARD.py
+++ ARD/genARD.py
@@ -4,11 +4,20 @@ import genCANJSON as genCAN
 
 if __name__ == "__main__":
     os.chdir("ARD/")
+    
+    # CAN
     latexCAN, headerCAN = genCAN.getConfig("config/CAN.json")
     with open ("tex/CANIDs.tex", "w") as CAN_TEX:
         CAN_TEX.write(latexCAN)
-    with open ("include/CANIDs.h", "w") as CAN_HEADER:
+    with open ("../include/CANIDs.h", "w") as CAN_HEADER:
         CAN_HEADER.write(headerCAN)
+
+    # EEPROM
+    latexEEPROM = genEEPROM.genEEPROM("config/EEPROM.json")
+    with open ("tex/EEPROM.tex", "w") as EEPROM_TEX:
+        EEPROM_TEX.write(latexEEPROM)
+
+    # Generate Tex
     if (os.system("xelatex --halt-on-error ARD.tex") != 0):
         print("Failed to create ARD.pdf.")
     else:
